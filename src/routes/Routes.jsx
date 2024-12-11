cb39336ab278d87dca24c2c09159b8a6e180ae5e
md-zonaed <shohag244044@gmail.com>
2024-12-11 11:06:42 +0600

    first commit

---
+++ src/routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const routers = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world!</div>,
+      },
+]);
+
+export default routers
